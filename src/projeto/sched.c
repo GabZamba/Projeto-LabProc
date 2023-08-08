3ff054dcd10441cfaf09d80d5c31b3fe86d9d1c5
GabZamba <gabrielzsb.comp@usp.br>
2023-08-07 22:39:56 -0300

    refatoração para melhorar legibilidade

--- src/projeto/sched.c
+++ src/projeto/sched.c
@@ -8,75 +8,77 @@
 Scheduler scheduler = {};
 
 // Pontos de entrada dos threads
-void *main(void *);
+extern void *main(void *);
 
 void stop(void); // rótulo stop no interrupt.s
 
-volatile int tid; // identificador do thread atual (0 ou 1)
+volatile int tid; // identificador do thread atual
 tcb_t *curr_tcb;  // tcb do thread atual
 Buffer *curr_buffer;
 
-uint8_t QuantumData[SCHEDULER_SIZE] = {10, 8, 6, 4};
+uint8_t QuantumData[SCHEDULER_SIZE] = {5, 4, 3, 2};
 
+/**
+ * Obtém a próxima thread a ser executada
+ *
+ * @param scheduler Ponteiro para o scheduler principal
+ * @param nextBuffer Ponteiro para o próximo Buffer (será definido pela função)
+ * @param nextThread Ponteiro para a próxima thread a ser executada (será definido pela função)
+ */
 void getNextThread(Scheduler *scheduler, Buffer **nextBuffer, tcb_t **nextThread)
 {
-    bool allEmpty = true;
-    // verifica se todos os buffers estao vazios
+    bool allBuffersEmpty = true;
+    bool allQuantumConsumed = true;
+
+    Buffer *buffers = &scheduler->buffers[0];
+
+    // verifica se todos os buffers estao vazios ou já tiveram seu quantum esgotado
     for (int i = 0; i < SCHEDULER_SIZE; i++)
     {
-        if (!scheduler->buffers[i].isEmpty) // se algum não está vazio
-        {
-            allEmpty = false;
-            break;
-        }
+        // verifica se há buffers não-vazios, zerando os quantums dos vazios
+        if (buffers[i].isEmpty)
+            buffers[i].quantumCnt = 0;
+        else
+            allBuffersEmpty = false;
+        // verifica se há quantums a serem executados
+        if (buffers[i].quantumCnt != 0)
+            allQuantumConsumed = false;
     }
 
     // se todos estiverem vazios, retorna ao reset (pc e lr são 0)
-    if (allEmpty)
+    if (allBuffersEmpty)
     {
-        *nextBuffer = &(scheduler->buffers[0]);
+        *nextBuffer = &buffers[0];
         *nextThread = &((*nextBuffer)->queue[(*nextBuffer)->start]);
         (*nextThread)->cpsr = 0x13;
         return;
     }
 
-    // se algum não estiver vazio, tenta pegar a próxima thread,
-    // só consegue se algum quantumCnt != 0
-    for (int i = 0; i < SCHEDULER_SIZE; i++)
+    // caso necessário, reinicia os quantumCnt
+    if (allQuantumConsumed)
     {
-        // itera pelo scheduler, selecionando o primeiro buffer que não está vazio
-        *nextBuffer = &(scheduler->buffers[i]);
-        // se estiver vazio ou já tiver sido percorrido, vai à próxima iteração
-        if ((*nextBuffer)->isEmpty || (*nextBuffer)->quantumCnt == 0)
+        for (int i = 0; i < SCHEDULER_SIZE; i++)
         {
-            (*nextBuffer)->quantumCnt = 0;
-            continue;
+            buffers[i].quantumCnt = buffers[i].quantumData;
         }
-        *nextThread = &((*nextBuffer)->queue[(*nextBuffer)->start]);
-        (*nextBuffer)->quantumCnt -= 1;
-        return;
-    }
-
-    // chega aqui caso todas tenham quantumCnt == 0
-    // reinicia os quantumCnt
-    for (int i = 0; i < SCHEDULER_SIZE; i++)
-    {
-        scheduler->buffers[i].quantumCnt = scheduler->buffers[i].quantumData;
     }
 
     // pega a proxima thread
     for (int i = 0; i < SCHEDULER_SIZE; i++)
     {
         // itera pelo scheduler, selecionando o primeiro buffer que não está vazio
-        *nextBuffer = &(scheduler->buffers[i]);
+        *nextBuffer = &buffers[i];
+
         // se estiver vazio ou já tiver sido percorrido, vai à próxima iteração
         if ((*nextBuffer)->isEmpty || (*nextBuffer)->quantumCnt == 0)
         {
             (*nextBuffer)->quantumCnt = 0;
             continue;
         }
+
         *nextThread = &((*nextBuffer)->queue[(*nextBuffer)->start]);
         (*nextBuffer)->quantumCnt -= 1;
+
         return;
     }
 }
@@ -88,10 +90,7 @@ void initializeScheduler(void)
         initBuffer(&(scheduler.buffers[i]), QuantumData[i]);
     }
 
-    uint8_t id = 0;
-    uint8_t *threadId = &id;
-
-    thread_create(threadId, 0, main, NULL);
+    thread_create(NULL, 0, main, NULL);
 
     getNextThread(&scheduler, &curr_buffer, &curr_tcb);
 
@@ -101,8 +100,9 @@ void initializeScheduler(void)
 }
 
 /**
- * Escalador:
- * Escolhe o próximo thread.
+ * Escalador: escolhe o próximo thread.
+ * 
+ * @param enqueueAgain Boolean que indica se a thread atual deve ou não ser reenfileirada
  */
 void schedule(bool enqueueAgain)
 {
@@ -120,9 +120,9 @@ void schedule(bool enqueueAgain)
             stop();
     }
 
+    // obtém a próxima thread a ser executada (salvando em curr_tcb)
     getNextThread(&scheduler, &curr_buffer, &curr_tcb);
 
-    // gets the next executing thread
     tid = curr_tcb->tid;
 
     setDisplayNumber(tid);
